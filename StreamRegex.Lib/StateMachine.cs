027069d01492e740903cc919f36a97542b1682ec
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-03-25 04:39:16 -0700

    Fix bug with 0 length files. Fix for reading padded 0s in buffer. Rename method more appropriately.

--- StreamRegex.Lib/StateMachine.cs
+++ StreamRegex.Lib/StateMachine.cs
@@ -12,20 +12,20 @@ public class StateMachine
         _states = states;
     }
 
-    public long GetMatchPosition(Stream toMatch)
+    public long GetFirstMatchPosition(Stream toMatch)
     {
         var curState = _states[0]; 
         byte[] buffer = new byte[_bufferSize];
         long resultPosition = toMatch.Position;
         long counter = 0;
         var numBytes = toMatch.Read(buffer, 0, _bufferSize);
-        while (numBytes != -1)
+        while (numBytes > 0)
         {
-            foreach (byte character in buffer)
+            for (int byteNum = 0; byteNum < numBytes; byteNum++)
             {
                 counter++;
-                var nextState = curState.Transition((char) character);
-                if (nextState == _states[0] && !curState.Accepts((char)character))
+                var nextState = curState.Transition((char)buffer[byteNum]);
+                if (nextState == _states[0] && !curState.Accepts((char)buffer[byteNum]))
                 {
                     resultPosition += counter;
                     counter = 0;
