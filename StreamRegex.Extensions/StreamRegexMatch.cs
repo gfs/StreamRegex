0a08cc77ac01a4a123e5c8ebbb446e01145a6429
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-10-02 14:45:58 -0700

    Docfx doesn't properly support inheritdoc

--- StreamRegex.Extensions/StreamRegexMatch.cs
+++ StreamRegex.Extensions/StreamRegexMatch.cs
@@ -3,13 +3,12 @@
 namespace StreamRegex.Extensions;
 
 /// <summary>
-/// Holds a from a <see cref="Regex"/>.
+/// A sliding buffer match from a <see cref="Regex"/>.
 /// </summary>
 /// <param name="Engine">The <see cref="Regex"/> which produced this match, or null if <paramref name="Success"/> is false</param>
 /// <param name="Success">If them match was successful.</param>
 /// <param name="Index">If <paramref name="Success"/> is true, the index in the Stream where the match was found. Otherwise -1.</param>
 /// <param name="Value">If <paramref name="Success"/> is true, the value in the Stream that was matched. Otherwise null.</param>
-/// <inheritdoc cref="SlidingBufferMatch"/>
 public record StreamRegexMatch(Regex? Engine = null, bool Success = false, long Index = -1, string? Value = null) : SlidingBufferMatch(Success, Index, Value)
 {
 }
