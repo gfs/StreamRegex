bb5fce9ba0e91e97536d3b0bf6c89cea208dd48c
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-10-06 21:09:44 -0700

    Add more GetRange apis
    Start, End API and API to fetch with a sliding buffer match.

--- StreamRegex.Extensions/Indexing/IndexExtensions.cs
+++ StreamRegex.Extensions/Indexing/IndexExtensions.cs
@@ -8,6 +8,47 @@ namespace StreamRegex.Extensions.Indexing;
 /// </summary>
 public static class IndexingExtensions
 {
+
+    /// <summary>
+    /// Gets a Span of chars from the target StreamReader
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="match"></param>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException">If the BaseStream of the StreamReader is not seekable</exception>
+    public static Span<char> GetRange(this StreamReader target, SlidingBufferMatch match)
+    {
+        if (!match.Success || match.Index == -1 || match.Length == -1)
+        {
+            throw new ArgumentException("Match must be a success and have an Index and Length set.");
+        }
+        target.BaseStream.Position = 0;
+        return target.GetRange(match.Index, match.Index + match.Length);
+    }
+
+    /// <summary>
+    /// Gets a Span of chars from the StreamReader
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
+    public static Span<char> GetRange(this StreamReader target, long start, long end)
+    {
+        Span<char> theSpan = new Span<char>(new char[start-end]);
+        if (target.BaseStream.CanSeek)
+        {
+            target.BaseStream.Position = start;
+        }
+        else
+        {
+            Span<char> overhead = new Span<char>(new char[start]);
+            _ = target.Read(overhead);
+        }
+        var readBytes = target.Read(theSpan);
+        return theSpan[..readBytes];
+    }
+    
     /// <summary>
     /// Get a <see cref="Span{T}"/> of <see cref="char"/> from the provided <paramref name="target"/> <see cref="StreamReader"/>
     /// </summary>
@@ -24,14 +65,37 @@ public static class IndexingExtensions
         {
             throw new IndexOutOfRangeException();
         }
-        Span<char> theSpan = new Span<char>(new char[endIndex-startIndex]);
-        if (target.BaseStream.CanSeek)
+
+        return GetRange(target, startIndex, endIndex);
+    }
+    
+    /// <summary>
+    /// Gets a Span of bytes based on the match
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="match"></param>
+    /// <returns></returns>
+    /// <exception cref="NotSupportedException">If the Stream is not seekable</exception>
+    public static Span<byte> GetRange(this Stream target, SlidingBufferMatch match)
+    {
+        if (!match.Success || match.Index == -1 || match.Length == -1)
         {
-            target.BaseStream.Position = startIndex;
+            throw new ArgumentException("Match must be a success and have an Index and Length set.");
+        }
+        target.Position = 0;
+        return target.GetRange(match.Index, match.Index + match.Length);
+    }
+
+    public static Span<byte> GetRange(this Stream target, long start, long end)
+    {
+        Span<byte> theSpan = new Span<byte>(new byte[start-end]);
+        if (target.CanSeek)
+        {
+            target.Position = start;
         }
         else
         {
-            Span<char> overhead = new Span<char>(new char[startIndex]);
+            Span<byte> overhead = new Span<byte>(new byte[start]);
             _ = target.Read(overhead);
         }
         var readBytes = target.Read(theSpan);
@@ -54,17 +118,7 @@ public static class IndexingExtensions
         {
             throw new IndexOutOfRangeException();
         }
-        Span<byte> theSpan = new Span<byte>(new byte[endIndex-startIndex]);
-        if (target.CanSeek)
-        {
-            target.Position = startIndex;
-        }
-        else
-        {
-            Span<byte> overhead = new Span<byte>(new byte[startIndex]);
-            _ = target.Read(overhead);
-        }
-        var readBytes = target.Read(theSpan);
-        return theSpan[..readBytes];
+
+        return GetRange(target, startIndex, endIndex);
     }
 }
